5a196e3225c49e57104b9433328ae210bf223704
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-02 23:42:15 +0700

    Add Init Route

--- Server.js
+++ Server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 5000;
 Routes(app);
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -5,5 +5,8 @@ const Routes = function (app) {
   app.use("/fliterlist", FitlerCatologeCrawl);
   app.use("/movie", Movie);
   app.use("/find", Find);
+  app.use("/",(req,res)=>{
+    res.send("Hi This Is Root Directory")
+  })
 };
 module.exports = Routes;
